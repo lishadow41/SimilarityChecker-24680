cbf6e0d40a3085d4a91cd3c720bcba9be165143c
HyunMin Cho <hm1041.cho@samsung.com>
2024-05-31 09:51:38 +0900

    [feature] implement length check
    Signed-off-by: HyunMin Cho <hm1041.cho@samsung.com>

--- TDD_SimilarityChecker/similarity_checker.cpp
+++ TDD_SimilarityChecker/similarity_checker.cpp
@@ -1,7 +1,28 @@
+#include <string>
+using namespace std;
 
 class SimilarityChecker
 {
 public:
 	SimilarityChecker() = default;
 	virtual ~SimilarityChecker() = default;
+
+public:
+	int check(const string& a, const string& b) {
+		if (a.length() == b.length()) return 60;
+
+		int longLength = a.length();
+		int shortLength = b.length();
+		if (longLength < shortLength) {
+			int temp = longLength;
+			longLength = shortLength;
+			shortLength = temp;
+		}
+
+		if (longLength / 2 >= shortLength) return 0;
+
+		int gap = longLength - shortLength;
+		int score = 60 - 60 * gap / shortLength;
+		return score;
+	}
 };
--- Test_SimilarityChecker/test.cpp
+++ Test_SimilarityChecker/test.cpp
@@ -1,7 +1,26 @@
 #include "pch.h"
 #include "../TDD_SimilarityChecker/similarity_checker.cpp"
 
-TEST(SimilarityChecker, Empty) {
+TEST(SimilarityChecker, SameLength) {
 	SimilarityChecker checker;
-	EXPECT_EQ(1, 1);
+	int n = checker.check("ABD", "GED");
+	EXPECT_EQ(n, 60);
+}
+
+TEST(SimilarityChecker, LengthScoreZero) {
+	SimilarityChecker checker;
+	int n = checker.check("A", "BB");
+	EXPECT_EQ(n, 0);
+}
+
+TEST(SimilarityChecker, LengthPartScore1) {
+	SimilarityChecker checker;
+	int n = checker.check("AAABB", "BAA");
+	EXPECT_EQ(n, 20);
+}
+
+TEST(SimilarityChecker, LengthPartScore2) {
+	SimilarityChecker checker;
+	int n = checker.check("AA", "AAE");
+	EXPECT_EQ(n, 30);
 }
